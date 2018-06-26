495e5d89fb86779bda50d6e753843f5456f3cbd3
Jatin Kathrotiya <jatin.k@simform.in>
2018-06-26 16:08:54 +0530

    relsoved Parse error

--- HMSegmentedControl/HMSegmentedControl.h
+++ HMSegmentedControl/HMSegmentedControl.h
@@ -255,8 +255,9 @@ typedef NS_ENUM(NSInteger, HMSegmentedControlImagePosition) {
 - (id)initWithSectionTitles:(NSArray<NSString *> *)sectiontitles;
 - (id)initWithSectionImages:(NSArray<UIImage *> *)sectionImages sectionSelectedImages:(NSArray<UIImage *> *)sectionSelectedImages;
 - (instancetype)initWithSectionImages:(NSArray<UIImage *> *)sectionImages sectionSelectedImages:(NSArray<UIImage *> *)sectionSelectedImages titlesForSections:(NSArray<NSString *> *)sectiontitles;
-- (void)setSelectedSegmentIndex:(NSUInteger)index animated:(BOOL)animated;
 - (void)setSelectedSegmentIndex:(NSUInteger)index animated:(BOOL)animated notify:(BOOL)notify;
+
+- (void)setSelectedSegmentIndex:(NSUInteger)index animated:(BOOL)animated;
 - (void)setIndexChangeBlock:(IndexChangeBlock)indexChangeBlock;
 - (void)setTitleFormatter:(HMTitleFormatterBlock)titleFormatter;
 
